e9df60bd3a3dc6f0be1742f25bfb4ed6b27eff9b
pierre <pierre.dasilva@ynov.com>
2020-02-17 23:18:01 +0100

    php

---
+++ Php/laragon/www/session2/pages/loisirs.php
@@ -0,0 +1,21 @@
+<?php include 'includes/data.php' ?>
+<!doctype html>
+<html>
+<head>
+<link rel="shortcut icon" href="favicon.ico">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+<?=	recupereBaseDonneesLoisirs() ?>
+<!--
+	<ul>
+  <?php foreach($loisirs as $l): ?>
+  <li> <span class="loisirs_label">
+    <?= $l ?>
+    </span> </li>
+  <?php endforeach ?>
+</ul>
+-->
+</body>
+</html>
