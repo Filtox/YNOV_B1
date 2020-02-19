4c19f1fe2af996ab45247f2eeb942231b6673070
pierre <pierre.dasilva@ynov.com>
2020-02-19 18:36:32 +0100

    dev2

--- Php/laragon/www/session/pages/main.php
+++ Php/laragon/www/session/pages/main.php
@@ -1,3 +1,4 @@
+<!--
 <!doctype html>
 <html>
 <head>
@@ -7,4 +8,52 @@
 <body>
     <?= recupereBaseDonneesPresentation() ?>
 </body>
+</html>
+-->
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<?php
+    /*
+		$mysqli = new mysqli('localhost', 'root', '', 'sitecvphp');
+		$mysqli->set_charset("utf8");
+		$requete = 'SELECT * FROM presentation';
+		$resultat = $mysqli->query($requete);
+		while ($ligne = $resultat->fetch_assoc()) {
+			echo $ligne['firstname'].' '.$ligne['lastname'].' '.$ligne['age'].' ';
+			echo $ligne['adresse'].' '.$ligne['ville'].' '.$ligne['mail'].' '.$ligne['phone'].' '.$ligne['permis'].'<br>';
+		}
+		$mysqli->close();
+		*/
+        
+        
+        $mysqli = new mysqli('192.20.27.12', 'root', 'HaLo1Bt', 'db_forum');
+
+if ($mysqli->connect_errno) {
+  die('<p>Connexion impossible : '.$mysqli->connect_error.'</p>');
+}
+
+$result = $mysqli->query('SELECT nom, prenom FROM users;') ;
+
+if (!$result) {
+  die('<p>ERREUR Requête invalide : '.$mysqli->error.'</p>');
+}
+
+while ($row = $result->fetch_assoc()) {
+  $nom = $row['nom'] ;
+  $prenom = $row['prenom'] ;
+  echo '<p>'.$prenom.' '.$nom.'</p>'."\r\n" ;
+}
+
+$result->free() ;
+
+$mysqli->close() ;
+
+        ?>
+        
+	</body> 
 </html>
